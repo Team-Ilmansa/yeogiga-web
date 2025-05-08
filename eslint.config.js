f2347279dc5158312ed0e2892cb24286803f8e26
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-05-08 16:31:47 +0900

    Chore(IL-64): Prettier, ESLint 설정 (#8)

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default [
   { ignores: ['dist'] },
@@ -16,18 +18,26 @@ export default [
         sourceType: 'module',
       },
     },
+    settings: {
+      react: { version: '19.0.0' },
+    },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      'prettier/prettier': 'error',
     },
   },
 ]
