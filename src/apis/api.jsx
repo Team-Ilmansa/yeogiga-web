95debcdbdf6978967bc67fc49ab5579bbcbf1a2f
joohee kim <dev.joohee0916@gmail.com>
2025-05-10 23:33:42 +0900

    feat: baseURL 설정

--- src/apis/api.jsx
+++ src/apis/api.jsx
@@ -2,7 +2,6 @@ import { baseUrl } from '@/config/Env'
 import axios from 'axios'
 
 const api = axios.create({
-  // TODO: 환경 변수로 설정하여 baseURL 숨기기
   baseURL: baseUrl,
   headers: {
     'Content-Type': 'application/json',
