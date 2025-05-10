3cc22081c54388ecfcdeddd789405cab7dc4de86
gugitgugit <rnwnsgur28@gmail.com>
2025-05-10 17:06:27 +0900

    refact: 라우팅 경로 및 컴포넌트명 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import Login from './pages/Login'
+import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
 const App = () => {
@@ -8,7 +8,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='login' element={<Login />} />
+        <Route path='signin' element={<SignIn />} />
         <Route path='signup' element={<SignUp />} />
       </Routes>
     </Router>
