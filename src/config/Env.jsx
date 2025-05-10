84f88f0c101d594fbbf68ffdd3cc1b00c5a90464
joohee kim <dev.joohee0916@gmail.com>
2025-05-10 23:33:11 +0900

    feat: 관련 환경변수 설정

---
+++ src/config/Env.jsx
@@ -0,0 +1,3 @@
+export const baseUrl = import.meta.env.VITE_SERVER_BASE_URL
+export const kakaoClientId = import.meta.env.VITE_KAKAO_REST_API_KEY
+export const kakaoRedirectUri = import.meta.env.VITE_KAKAO_REDIRECT_URI
