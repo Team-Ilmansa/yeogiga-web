7e24a9e5001e888a53a46ae70777931a0d830b2f
joohee kim <dev.joohee0916@gmail.com>
2025-05-11 02:21:20 +0900

    feat: 네이버 로그인 버튼 구현

--- src/components/sign-in/SignInButton.jsx
+++ src/components/sign-in/SignInButton.jsx
@@ -1,17 +1,24 @@
 import kakaoButton from '@/assets/kakaoButton.png'
-import { kakaoLoginUrl } from '@/config/Url'
+import naverButton from '@/assets/naverButton.png'
+import { kakaoLoginUrl, naverLoginUrl } from '@/config/Url'
 
-const SignInButton = () => {
-  return null
-}
+const SignInButton = () => null
+const ButtonStyle = 'mx-auto w-50 cursor-pointer'
 
 SignInButton.KakaoLogin = () => {
   return (
     <a href={kakaoLoginUrl}>
+      <img src={kakaoButton} alt='카카오 로그인' className={ButtonStyle} />
+    </a>
+  )
+}
+SignInButton.NaverLogin = () => {
+  return (
+    <a href={naverLoginUrl}>
       <img
-        src={kakaoButton}
-        alt='카카오 로그인'
-        className='mx-auto w-50 cursor-pointer'
+        src={naverButton}
+        alt='네이버 로그인'
+        className={`${ButtonStyle} mt-3`}
       />
     </a>
   )
