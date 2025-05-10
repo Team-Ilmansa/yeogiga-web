eff047ef50e25f6b5c81a7592dc2988d0809b576
gugitgugit <rnwnsgur28@gmail.com>
2025-05-10 17:06:15 +0900

    refact: 파일 및 컴포넌트명 변경

---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,11 @@
+import SignInForm from '@/components/SignIn/SignInForm'
+
+const SignIn = () => {
+  return (
+    <div className='flex h-screen w-screen items-center justify-center'>
+      <SignInForm />
+    </div>
+  )
+}
+
+export default SignIn
