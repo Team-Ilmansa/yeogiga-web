d2e1d3336e2add3d13356915b61ca78f3fe7f599
gugitgugit <rnwnsgur28@gmail.com>
2025-05-14 13:38:19 +0900

    feat: 아이디 중복확인 API 구현

---
+++ src/apis/authentication/usernameDupCheck.jsx
@@ -0,0 +1,23 @@
+import api from '@/apis/api'
+
+/**아이디 중복 체크 API */
+const usernameDupCheck = async (username) => {
+  try {
+    const response = await api.get('auth/dup-check/username', {
+      params: { value: username },
+    })
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message)
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default usernameDupCheck
