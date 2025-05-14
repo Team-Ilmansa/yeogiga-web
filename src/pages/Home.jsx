b59f796814a293085e4f179a3140c21bcf5ae414
gugitgugit <rnwnsgur28@gmail.com>
2025-05-14 14:30:01 +0900

    feat: 메인 화면에 로그인 상태 UI 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,36 @@
+import signOutApi from '@/apis/authentication/signOutApi'
+import useAuth from '@/hooks/useAuth'
+
 const Home = () => {
+  const { user, logout } = useAuth()
+
+  /**버튼 클릭 시 로그아웃 로직
+   * TODO: 상단 NavBar로 옮기기 */
+  const handleSignOut = async () => {
+    try {
+      const result = await signOutApi()
+      console.log('로그아웃 성공:', result)
+      logout()
+    } catch (error) {
+      console.error('로그아웃 중 오류 발생: ', error.message)
+    }
+  }
+
   return (
-    <nav className='flex h-screen w-screen items-center justify-center gap-2'>
-      <button>
-        <a href='/signup'>회원가입</a>
-      </button>
-      <button>
-        <a href='/signin'>로그인</a>
-      </button>
-    </nav>
+    <div className='flex h-screen w-screen flex-col items-center justify-center'>
+      <h1 className='text-3xl'>
+        {user ? `안녕하세요! ${user.nickname}님!` : '로그인해 주세요'}
+      </h1>
+      {user && <button onClick={handleSignOut}>로그아웃</button>}
+      <nav className='mt-5 flex gap-2'>
+        <button>
+          <a href='/signup'>회원가입</a>
+        </button>
+        <button>
+          <a href='/signin'>로그인</a>
+        </button>
+      </nav>
+    </div>
   )
 }
 
