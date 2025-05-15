4d89acd5909d099e292d84c2eb06c9cd9639b072
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-05-15 23:02:45 +0900

    Feature(IL-108): 닉네임 변경 (#27)

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,9 +1,12 @@
+import updateNicknameApi from '@/apis/users/updateNicknameApi'
 import userInfoApi from '@/apis/users/userInfoApi'
 import useAuth from '@/hooks/useAuth'
 import React, { useEffect, useState } from 'react'
 
 const MyPage = () => {
   const [userInfo, setUserInfo] = useState([])
+  const [newNickname, setNewNickname] = useState('')
+  const [isNicknameInputOpen, setIsNicknameInputOpen] = useState(false)
 
   /**회원 정보 조회 API 호출 */
   const fetchUserInfo = async () => {
@@ -17,13 +20,47 @@ const MyPage = () => {
     }
   }
 
+  /**닉네임 변경 API 호출 */
+  const updateNickname = async (e) => {
+    e.preventDefault()
+    try {
+      const result = await updateNicknameApi({ nickname: newNickname })
+      console.log('닉네임 변경 성공: ', result)
+      alert('닉네임이 성공적으로 변경되었습니다!')
+      fetchUserInfo()
+      setIsNicknameInputOpen(false)
+    } catch (err) {
+      console.error('닉네임 변경 에러: ', err)
+      alert(err.message)
+    }
+  }
+
+  /**닉네임 입력 창 출력 상태 토글 */
+  const toggleNicknameInput = () => {
+    setIsNicknameInputOpen((prev) => !prev)
+    setNewNickname('')
+  }
+
   useEffect(() => {
     fetchUserInfo()
   }, [])
 
   return (
-    <div className='flex h-screen w-screen flex-col items-center justify-center'>
+    <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
       <p>닉네임: {userInfo.nickname}</p>
+      <button onClick={toggleNicknameInput}>닉네임 변경</button>
+      {isNicknameInputOpen && (
+        <form onSubmit={updateNickname} className='flex gap-2'>
+          <input
+            placeholder='새로운 닉네임을 입력해주세요'
+            value={newNickname}
+            onChange={(e) => setNewNickname(e.target.value)}
+            className='w-75'
+          />
+          <button type='submit'>확인</button>
+        </form>
+      )}
+
       <p>이메일: {userInfo.email}</p>
     </div>
   )
