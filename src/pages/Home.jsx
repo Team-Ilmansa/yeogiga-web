8e8249a16316f05121b696a7f85531ee696977c2
gugitgugit <rnwnsgur28@gmail.com>
2025-05-17 00:56:40 +0900

    feat: 여행 생성 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,14 @@
 import signOutApi from '@/apis/authentication/signOutApi'
+import createTripApi from '@/apis/trip/createTripApi'
 import useAuth from '@/hooks/useAuth'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const Home = () => {
   const { user, logout } = useAuth()
+  const [isCreateTripInputOpen, setIsCreateTripInputOpen] = useState(false)
+  const [tripTitle, setTripTitle] = useState('')
+  const [tripCity, setTripCity] = useState('')
 
   /**버튼 클릭 시 로그아웃 로직
    * TODO: 상단 NavBar로 옮기기 */
@@ -15,6 +20,27 @@ const Home = () => {
     } catch (error) {
       console.error('로그아웃 중 오류 발생: ', error.message)
     }
+  }
+
+  /**여행 생성 API 호출 */
+  const createTrip = async (e) => {
+    e.preventDefault()
+    try {
+      const result = await createTripApi({ title: tripTitle, city: tripCity })
+      console.log('여행 생성 성공: ', result)
+      alert('여행이 성공적으로 생성되었습니다!')
+      setIsCreateTripInputOpen(false)
+    } catch (err) {
+      console.log('여행 생성 실패: ', err)
+      alert(err.message)
+    }
+  }
+
+  /**여행 생성 창 출력 상태 토글 */
+  const toggleCreateTripInput = () => {
+    setIsCreateTripInputOpen((prev) => !prev)
+    setTripTitle('')
+    setTripCity('')
   }
 
   return (
@@ -30,6 +56,7 @@ const Home = () => {
             <Link to='/mypage' className='link'>
               마이 페이지
             </Link>
+            <button onClick={toggleCreateTripInput}>여행 생성하기</button>
           </>
         ) : (
           <>
@@ -42,6 +69,31 @@ const Home = () => {
           </>
         )}
       </nav>
+      {isCreateTripInputOpen && (
+        <fieldset className='rounded-2xl border p-4'>
+          <legend className='p-2'>여행 생성</legend>
+          <form
+            onSubmit={createTrip}
+            className='flex flex-col items-center justify-center gap-2'
+          >
+            <input
+              placeholder='여행 이름을 입력해주세요.'
+              value={tripTitle}
+              onChange={(e) => setTripTitle(e.target.value)}
+              className='w-75'
+              type='text'
+            />
+            <input
+              placeholder='여행 갈 목적지를 입력해주세요'
+              value={tripCity}
+              onChange={(e) => setTripCity(e.target.value)}
+              className='w-75'
+              type='text'
+            />
+            <button type='submit'>생성하기</button>
+          </form>
+        </fieldset>
+      )}
     </div>
   )
 }
