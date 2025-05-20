7018999e8470a73f38764f4d188c7b9ed16f1a36
gugitgugit <rnwnsgur28@gmail.com>
2025-05-20 22:30:13 +0900

    feat: 소셜 로그인 성공 시 메인 화면으로 이동

--- src/components/sign-in/NaverRedirect.jsx
+++ src/components/sign-in/NaverRedirect.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 import oauthSignInApi from '@/apis/authentication/oauthSignInApi'
 import useAuth from '@/hooks/useAuth'
 
 const NaverRedirect = () => {
+  const navigate = useNavigate()
   const location = useLocation()
   const { login } = useAuth()
 
@@ -22,6 +23,7 @@ const NaverRedirect = () => {
       const response = await oauthSignInApi('NAVER', code)
       login({ token: response.data.token.accessToken })
       console.log('네이버 로그인 성공', response)
+      navigate('/')
     } catch (error) {
       console.error('네이버 로그인 중 오류 발생:', error.message)
     }
