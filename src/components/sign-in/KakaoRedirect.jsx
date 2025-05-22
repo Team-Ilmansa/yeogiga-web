a79af3154eb74bdb73a98218e01f1f2f9790bf51
joohee kim <dev.joohee0916@gmail.com>
2025-05-22 22:56:37 +0900

    feat: oauth 로그인 후 스토리지에 state 전달

--- src/components/sign-in/KakaoRedirect.jsx
+++ src/components/sign-in/KakaoRedirect.jsx
@@ -9,24 +9,26 @@ const KakaoRedirect = () => {
   const { login } = useAuth()
 
   useEffect(() => {
-    const queryPrams = new URLSearchParams(location.search)
-    const code = queryPrams.get('code')
+    const queryParams = new URLSearchParams(location.search)
+    const code = queryParams.get('code')
 
     if (code) {
       fetchKakaoAccessToken(code)
     }
   }, [location])
 
-  /**카카오 소셜 로그인 호출 */
+  /** 카카오 소셜 로그인 호출 */
   const fetchKakaoAccessToken = async (code) => {
     try {
       const response = await oauthSignInApi('KAKAO', code)
       login({ token: response.data.token.accessToken })
+      sessionStorage.setItem('fromOAuth', 'true')
       navigate('/')
     } catch (error) {
       console.error('카카오 로그인 중 오류 발생:', error.message)
     }
   }
+  return null
 }
 
 export default KakaoRedirect
--- src/components/sign-in/NaverRedirect.jsx
+++ src/components/sign-in/NaverRedirect.jsx
@@ -9,24 +9,27 @@ const NaverRedirect = () => {
   const { login } = useAuth()
 
   useEffect(() => {
-    const queryPrams = new URLSearchParams(location.search)
-    const code = queryPrams.get('code')
+    const queryParams = new URLSearchParams(location.search)
+    const code = queryParams.get('code')
 
     if (code) {
       fetchNaverAccessToken(code)
     }
   }, [location])
 
-  /**네이버 소셜 로그인 호출 */
+  /** 네이버 소셜 로그인 호출 */
   const fetchNaverAccessToken = async (code) => {
     try {
       const response = await oauthSignInApi('NAVER', code)
       login({ token: response.data.token.accessToken })
+      sessionStorage.setItem('fromOAuth', 'true')
       navigate('/')
     } catch (error) {
       console.error('네이버 로그인 중 오류 발생:', error.message)
     }
   }
+
+  return null
 }
 
 export default NaverRedirect
