36a572453487ad3b782ceb7d5a7bd75b7b685890
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-06-03 13:20:28 +0900

    Feature(IL-174): 여행 이름 수정 (#41)

--- src/components/trip/TripInfo.jsx
+++ src/components/trip/TripInfo.jsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import updateTripTitleApi from '@/apis/trip/updateTripTitleApi'
+import React, { useState } from 'react'
 
 const TripInfo = ({ tripInfo }) => {
+  const [newTitle, setNewTitle] = useState('')
+  const [isUpdateTitleInputOpen, setIsUpdateTitleInputOpen] = useState(false)
+
+  /**여행 이름 변경 창 출력 상태 토글 */
+  const toggleUpdateTitleInput = () => {
+    setIsUpdateTitleInputOpen((prev) => !prev)
+    setNewTitle('')
+  }
+
+  /**여행 이름 변경 API 호출 */
+  const updateTitle = async (e) => {
+    e.preventDefault()
+    try {
+      const result = await updateTripTitleApi(tripInfo.tripId, {
+        title: newTitle,
+      })
+      alert('여행 이름이 성공적으로 변경되었습니다!')
+      setIsUpdateTitleInputOpen(false)
+      window.location.reload()
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
     <div>
       {tripInfo ? (
         <div>
           <fieldset className='rounded-2xl border p-4'>
             <legend className='p-2'>여행 정보</legend>
-            <h3>제목: {tripInfo.title}</h3>
+            <div className='flex justify-between'>
+              <h3>제목: {tripInfo.title}</h3>
+              <button className='px-2 py-0' onClick={toggleUpdateTitleInput}>
+                수정
+              </button>
+            </div>
+            {isUpdateTitleInputOpen && (
+              <fieldset className='rounded-2xl border p-4'>
+                <legend className='p-2'>여행 이름</legend>
+                <form
+                  onSubmit={updateTitle}
+                  className='flex flex-col items-center justify-center gap-2'
+                >
+                  <input
+                    placeholder='변경할 여행 이름을 입력해주세요.'
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    className='w-75'
+                    type='text'
+                  />
+                  <button type='submit'>변경하기</button>
+                </form>
+              </fieldset>
+            )}
             <p>도시: {tripInfo.city}</p>
             <p>
               기간: {new Date(tripInfo.startedAt).toLocaleString()} ~{' '}
