d848e4327b2fec48ebaa2684dbeb10e63e091ce9
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-06-11 12:48:27 +0900

    Feature(IL-181): 나의 W2M 캘린더 수정 (#50)

---
+++ src/apis/calendar/updateMyCalendarApi.jsx
@@ -0,0 +1,21 @@
+import api from '@/apis/api'
+
+/**나의 W2M 캘린더 수정 API */
+const updateMyCalendarApi = async (tripId, body) => {
+  try {
+    const response = await api.put(`trip/${tripId}/calendars`, body)
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message)
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default updateMyCalendarApi
