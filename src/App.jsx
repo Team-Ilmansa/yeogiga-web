5badd0d259f605596524cafbc470b625fd91119a
gugitgugit <rnwnsgur28@gmail.com>
2025-07-06 01:32:00 +0900

    feat: 웹앱 기본 UI 설정

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,27 @@ import Trip from './pages/Trip'
 import Participation from './pages/Participation'
 
 const App = () => {
+  /**TODO: 스크롤 방지 기능 추가 */
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='signin' element={<SignIn />} />
-        <Route path='signup' element={<SignUp />} />
-        <Route path='oauth/kakao/callback' element={<KakaoRedirect />} />
-        <Route path='oauth/naver/callback' element={<NaverRedirect />} />
-        <Route path='mypage' element={<MyPage />} />
-        <Route path='trip/:tripId' element={<Trip />} />
-        <Route path='trip/:tripId'>
-          <Route index element={<Trip />} />
-          <Route path='participation' element={<Participation />} />
-        </Route>
-      </Routes>
-    </Router>
+    <div className='flex min-h-screen justify-center bg-gray-100'>
+      <div className='min-h-screen w-full max-w-5xl bg-white px-10'>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='signin' element={<SignIn />} />
+            <Route path='signup' element={<SignUp />} />
+            <Route path='oauth/kakao/callback' element={<KakaoRedirect />} />
+            <Route path='oauth/naver/callback' element={<NaverRedirect />} />
+            <Route path='mypage' element={<MyPage />} />
+            <Route path='trip/:tripId' element={<Trip />} />
+            <Route path='trip/:tripId'>
+              <Route index element={<Trip />} />
+              <Route path='participation' element={<Participation />} />
+            </Route>
+          </Routes>
+        </Router>
+      </div>
+    </div>
   )
 }
 
