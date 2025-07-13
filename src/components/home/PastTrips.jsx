fda83eed09cb8a2174f70e870859c4a68a141198
gugitgugit <rnwnsgur28@gmail.com>
2025-07-13 20:20:44 +0900

    feat: 지난 여행 컴포넌트 구현

---
+++ src/components/home/PastTrips.jsx
@@ -0,0 +1,32 @@
+/**지난여행 돌아보기 */
+const PastTrips = () => {
+  const imageList = [
+    'https://placehold.co/400x400?text=Spot+1',
+    'https://placehold.co/400x400?text=Spot+2',
+    'https://placehold.co/400x400?text=Spot+3',
+    'https://placehold.co/400x400?text=Spot+4',
+    'https://placehold.co/400x400?text=Spot+5',
+  ]
+
+  return (
+    <div className='w-full'>
+      <div className='pb-3 text-2xl font-bold text-[var(--Grey-Scale-grey-400)]'>
+        지난여행 돌아보기
+      </div>
+
+      {/* 장소 이미지 */}
+      <div className='scrollbar-hide flex gap-4 overflow-x-auto'>
+        {imageList.map((src, idx) => (
+          <img
+            key={idx}
+            src={src}
+            alt={`여행 이미지 ${idx + 1}`}
+            className='h-[400px] w-[400px] flex-shrink-0 rounded-xl object-cover'
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default PastTrips
