df27444049ae6514191e819015dadb3ab11317aa
joohee kim <dev.joohee0916@gmail.com>
2025-07-13 19:58:56 +0900

    chore: 환경변수 수정

--- src/config/Env.jsx
+++ src/config/Env.jsx
@@ -1,4 +1,5 @@
 export const baseUrl = import.meta.env.VITE_SERVER_BASE_URL
+export const prodBaseUrl = import.meta.env.VITE_PROD_SERVER_BASE_URL
 export const kakaoClientId = import.meta.env.VITE_KAKAO_REST_API_KEY
 export const kakaoRedirectUri = import.meta.env.VITE_KAKAO_REDIRECT_URI
 export const naverClientId = import.meta.env.VITE_NAVER_REST_API_KEY
