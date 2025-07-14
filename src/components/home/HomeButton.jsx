0ccdd0974a1c8d6a121bf8d13e14b32fe78bca48
gugitgugit <rnwnsgur28@gmail.com>
2025-07-15 01:10:25 +0900

    feat: 하단바 UI 조정

--- src/components/home/HomeButton.jsx
+++ src/components/home/HomeButton.jsx
@@ -6,15 +6,31 @@ import MyPageIcon from '@/assets/home/MyPageIcon'
 const HomeButton = () => {
   return (
     <div>
+      {/* 가운데 버튼 상단 그림자 생성을 위한 요소 */}
       <div className='fixed bottom-0 left-0 flex w-full justify-center'>
-        <div className='flex w-4xl items-center justify-around rounded-t-[20px] bg-[var(--Grey-Scale-grey-00)] p-[15px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
-          <HomeIcon color='var(--Blue-Scale-blue-500)' />
-          <div></div>
-          <MyPageIcon color='var(--Grey-Scale-grey-200)' />
+        <div className='flex w-4xl items-center justify-around p-[15px]'>
+          <HomeIcon color='var(--Blue-Scale-blue-500)' opacity={0} />
+          <div className='h-[150px] w-[150px] rounded-full shadow-[0_0_4px_rgba(0,0,0,0.10)]' />
+          <MyPageIcon color='var(--Grey-Scale-grey-200)' opacity={0} />
         </div>
       </div>
+
+      {/* 하단바 배경 */}
       <div className='fixed bottom-0 left-0 flex w-full justify-center'>
-        <img src={createButton} className='h-[150px] w-[150px]' />
+        <div className='flex w-4xl items-center justify-around rounded-t-[20px] bg-[var(--Grey-Scale-grey-00)] p-[15px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
+          <HomeIcon color='var(--Blue-Scale-blue-500)' opacity={0} />
+          <div></div>
+          <MyPageIcon color='var(--Grey-Scale-grey-200)' opacity={0} />
+        </div>
+      </div>
+
+      {/* 하단바 실제 버튼 */}
+      <div className='fixed bottom-0 left-0 flex w-full justify-center'>
+        <div className='flex w-4xl items-end justify-around rounded-t-[20px] p-[15px]'>
+          <HomeIcon color='var(--Blue-Scale-blue-500)' />
+          <img src={createButton} className='h-[150px] w-[150px]' />
+          <MyPageIcon color='var(--Grey-Scale-grey-200)' />
+        </div>
       </div>
     </div>
   )
