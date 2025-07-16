23adc143d2bcff229fa44824e5dab779219668de
gugitgugit <rnwnsgur28@gmail.com>
2025-07-17 00:07:17 +0900

    feat: 뒤로가기 버튼 구현

---
+++ src/assets/sign-up/GoBack.jsx
@@ -0,0 +1,22 @@
+/**뒤로가기 버튼 svg */
+const GoBack = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='50'
+      height='50'
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path
+        d='M15 6L9 12L15 18'
+        stroke='#313131'
+        stroke-width='2'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default GoBack
