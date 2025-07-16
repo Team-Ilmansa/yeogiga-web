cf1c483448c7e5d1041f52a1ced8266e18b2ece1
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-07-17 00:51:18 +0900

    Feature(IL-201): 회원가입 화면 - 이메일 인증 화면 UI 구현 (#65)

---
+++ src/components/sign-up/StepIndicator.jsx
@@ -0,0 +1,19 @@
+/**회원가입 절차 표시 */
+const StepIndicator = ({ step }) => {
+  return (
+    <div className='flex gap-5'>
+      {[1, 2, 3, 4].map((s) => (
+        <div
+          key={s}
+          className={`h-4 w-4 rounded-full ${
+            step >= s
+              ? 'bg-[var(--Blue-Scale-blue-500)]'
+              : 'bg-[var(--Grey-Scale-grey-100)]'
+          }`}
+        ></div>
+      ))}
+    </div>
+  )
+}
+
+export default StepIndicator
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,11 +1,7 @@
 import SignUpForm from '@/components/sign-up/SignUpForm'
 
 const SignUp = () => {
-  return (
-    <div className='flex h-screen w-screen items-center justify-center'>
-      <SignUpForm />
-    </div>
-  )
+  return <SignUpForm />
 }
 
 export default SignUp
