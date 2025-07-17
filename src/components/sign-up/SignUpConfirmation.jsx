181c99e7b9e0d9e5279e0eb458e18fe71e5f3a0d
gugitgugit <rnwnsgur28@gmail.com>
2025-07-17 15:28:12 +0900

    feat: 약관 동의 화면 구현

---
+++ src/components/sign-up/SignUpConfirmation.jsx
@@ -0,0 +1,20 @@
+/**회원가입 완료 화면 */
+const SignUpConfirmation = () => {
+  return (
+    <div>
+      <div className='mb-15 px-10'>
+        {/* 회원가입 완료 화면 상단 문구 */}
+        <div className='text-4xl/[1.4] font-bold text-[var(--Grey-Scale-grey-400)]'>
+          님
+          <br />
+          여기가 가입을 축하드려요!
+        </div>
+        <div className='text-lg/[2] text-[var(--Grey-Scale-grey-300)]'>
+          여기가와 함께 더 체계적인 단체 여행을 즐겨봐요
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUpConfirmation
