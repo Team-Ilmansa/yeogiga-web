7aaa9349ffc4de3f2bd6c0ee33788beb5bd72a8b
joohee kim <dev.joohee0916@gmail.com>
2025-07-20 01:37:25 +0900

    feat: 여행 타이틀 구현

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,32 @@
 import TripTitle from '@/components/dashboard/TripTitle'
+import GoBack from '@/assets/sign-up/GoBack'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const Dashboard = () => {
-  return (
-    // 뒤로가기 버튼
+  const navigate = useNavigate()
+  const { tripId } = useParams()
 
-    // 여행 타이틀
-    <div className='flex w-full flex-col gap-15 bg-[var(--Grey-Scale-grey-50)] pt-10 pb-50 pl-10'>
-      <TripTitle />
+  /**뒤로 가기 버튼 */
+  const handleBack = () => {
+    navigate(`/trip/${tripId}`)
+  }
+
+  return (
+    <div className='flex w-full flex-col pt-5'>
+      {/* 뒤로 가기 버튼 */}
+      <div>
+        <button
+          className='text-bold my-2 border-none px-4'
+          onClick={handleBack}
+        >
+          <GoBack />
+        </button>
+      </div>
+
+      {/* 여행 타이틀 */}
+      <div className='flex w-full flex-col gap-15 pt-10 pb-10 pl-10'>
+        <TripTitle />
+      </div>
     </div>
   )
 }
