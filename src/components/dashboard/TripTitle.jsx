aa2ab29fa5c0c101aa8442f4ea94f5b03a01614c
joohee kim <dev.joohee0916@gmail.com>
2025-07-20 02:17:41 +0900

    feat: 여행 도시 정보 조건부 렌더링 구현

--- src/components/dashboard/TripTitle.jsx
+++ src/components/dashboard/TripTitle.jsx
@@ -16,7 +16,6 @@ const TripTitle = () => {
   }
 
   useEffect(() => {
-    /**특정 여행 정보 조회 API 호출 */
     const fetchTripInfo = async () => {
       try {
         const result = await readTripInfoApi(tripId)
@@ -28,10 +27,12 @@ const TripTitle = () => {
 
     if (tripId) fetchTripInfo()
   }, [])
+
   return (
     <div>
       {tripInfo ? (
         <div>
+          {/* 상태 + 타이틀 */}
           <div>
             <div
               style={{ color: 'var(--Blue-Scale-blue-500)', fontSize: '14pt' }}
@@ -41,49 +42,57 @@ const TripTitle = () => {
             <div className='font-bold' style={{ fontSize: '28pt' }}>
               {tripInfo.title}
             </div>
-            <div className='mt-2'>
-              <div
-                className='flex items-center gap-2 text-sm text-gray-700'
-                style={{ fontSize: '14pt' }}
-              >
-                <CalendarIcon className='h-5 w-5' />
-                <span>
-                  {new Date(tripInfo.startedAt)
-                    .toLocaleDateString('ko-KR', {
-                      year: 'numeric',
-                      month: '2-digit',
-                      day: '2-digit',
-                    })
-                    .slice(0, -1)}{' '}
-                  -{' '}
-                  {new Date(tripInfo.endedAt)
-                    .toLocaleDateString('ko-KR', {
-                      year: 'numeric',
-                      month: '2-digit',
-                      day: '2-digit',
-                    })
-                    .slice(0, -1)}
-                </span>
-              </div>
-            </div>
           </div>
-          <div className='mt-1 flex items-center gap-2 text-sm text-gray-700'>
-            <UserIcon className='h-5 w-5' />
-            <ul className='flex gap-1'>
-              {tripInfo.members.map((member) => (
-                <li key={member.userId}>
-                  {member.imageUrl ? (
-                    <img
-                      src={member.imageUrl}
-                      alt={member.nickname}
-                      className='h-[20px] w-[20px] rounded-full object-cover'
-                    />
-                  ) : (
-                    <div className='bg-gray h-[20px] w-[20px] rounded-full bg-gray-300' />
-                  )}
-                </li>
-              ))}
-            </ul>
+
+          <div
+            className='mt-4 flex flex-col gap-y-1 text-sm text-gray-700'
+            style={{ fontSize: '14pt' }}
+          >
+            {/* 위치 */}
+            <div className='flex items-center gap-2'>
+              <PinIcon className='h-5 w-5' />
+              <div>{tripInfo.city || '???'}</div>
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <CalendarIcon className='h-5 w-5' />
+              <span>
+                {new Date(tripInfo.startedAt)
+                  .toLocaleDateString('ko-KR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })
+                  .slice(0, -1)}{' '}
+                -{' '}
+                {new Date(tripInfo.endedAt)
+                  .toLocaleDateString('ko-KR', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                  })
+                  .slice(0, -1)}
+              </span>
+            </div>
+
+            <div className='flex items-center gap-2'>
+              <UserIcon className='h-5 w-5' />
+              <ul className='flex gap-1'>
+                {tripInfo.members.map((member) => (
+                  <li key={member.userId}>
+                    {member.imageUrl ? (
+                      <img
+                        src={member.imageUrl}
+                        alt={member.nickname}
+                        className='h-[20px] w-[20px] rounded-full object-cover'
+                      />
+                    ) : (
+                      <div className='h-[20px] w-[20px] rounded-full bg-gray-300' />
+                    )}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       ) : (
