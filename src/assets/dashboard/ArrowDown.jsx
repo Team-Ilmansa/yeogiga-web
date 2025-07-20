21e374b8a5487278565f411508490b56abb301fd
joohee kim <dev.joohee0916@gmail.com>
2025-07-21 01:47:55 +0900

    feat: 화살표 컴포넌트 추가

---
+++ src/assets/dashboard/ArrowDown.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ArrowDown = () => {
+  return (
+    <div>
+      <svg
+        width='12'
+        height='8'
+        viewBox='0 0 12 8'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path
+          d='M1 1.5L6 6.5L11 1.5'
+          stroke='#7D7D7D'
+          stroke-width='1.66667'
+          stroke-linecap='round'
+          stroke-linejoin='round'
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default ArrowDown
