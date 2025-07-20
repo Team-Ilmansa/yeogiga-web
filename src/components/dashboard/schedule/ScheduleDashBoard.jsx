dab164181b1930b82d4e1a978842f1cb317c03d3
joohee kim <dev.joohee0916@gmail.com>
2025-07-21 01:48:54 +0900

    feat: 일정에 따른 탭 구현

---
+++ src/components/dashboard/schedule/ScheduleDashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DayTabs from './DayTabs'
+import DateBox from './\bDateBox'
+
+const ScheduleDashBoard = () => {
+  return (
+    <>
+      <DayTabs />
+    </>
+  )
+}
+
+export default ScheduleDashBoard
