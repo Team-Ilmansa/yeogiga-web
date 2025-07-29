ca9e0d113fca26ffa0a2952ef2b6445ff2e9f9f6
joohee kim <dev.joohee0916@gmail.com>
2025-07-30 02:13:38 +0900

    feat: 모달 상태 관리 구현

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,25 +4,25 @@ import { useNavigate, useParams } from 'react-router-dom'
 import SlideTabs from '@/components/dashboard/SlideTabs'
 import { useState, useEffect } from 'react'
 import KebabIcon from '@/assets/dashboard/KebabIcon'
-import KebabModal from '@/components/dashboard/modal/KebobModal'
 import readMyCalendarApi from '@/apis/calendar/readMyCalendarApi'
+import KebabModal from '@/components/dashboard/modal/KebabModal'
+import UpdateTitleModal from '@/components/dashboard/modal/UpdateTitleModal'
 
 /**여행 정보 대시보드 페이지 */
 const Dashboard = () => {
   const navigate = useNavigate()
   const { tripId } = useParams()
+  /** 케밥 모달 열림 여부 상태 */
+  const [isKebabOpen, setIsKebabOpen] = useState(false)
+  /** 여행 이름 수정 모달 열림 여부 상태 */
+  const [isUpdateTitleOpen, setIsUpdateTitleOpen] = useState(false)
 
   /**여행 일정 확정 여부 */
   const [isScheduleConfirmed, setIsScheduleConfirmed] = useState(false)
-  /**개인 여행 일정 등록 여부 */
-  const [isRegistred, setIsRegistred] = useState(true)
-  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const handleBack = () => {
     navigate(`/`)
   }
-  /**케밥버튼 모달창 토글*/
-  const toggleModal = () => setIsModalOpen((prev) => !prev)
 
   useEffect(() => {
     const fetchMyCalendar = async () => {
@@ -38,42 +38,61 @@ const Dashboard = () => {
   }, [])
 
   return (
-    <div className='flex w-full flex-col pt-5'>
-      <div className='mb-5 flex items-center justify-between px-8'>
-        <button className='border-none' onClick={handleBack}>
-          <GoBack />
-        </button>
-        {/** 케밥 버튼 클릭 시 모달창 열기 */}
-        <button
-          onClick={toggleModal}
-          className='border-none bg-transparent p-0 outline-none focus:outline-none'
-        >
-          <KebabIcon />
-        </button>
-      </div>
-      <div className='flex w-full flex-col gap-15 px-10'>
-        <TripTitle
-          isScheduleConfirmed={isScheduleConfirmed}
-          setIsScheduleConfirmed={setIsScheduleConfirmed}
+    <>
+      {/** 케밥 모달이 열렸을 때만 렌더링 */}
+      {isKebabOpen && (
+        <KebabModal
+          onClose={() => setIsKebabOpen(false)}
+          onOpenUpdateModal={() => {
+            setIsKebabOpen(false)
+            setIsUpdateTitleOpen(true)
+          }}
         />
-        <SlideTabs isScheduleConfirmed={isScheduleConfirmed} />
-      </div>
-      {/* 여행 날짜 확정 버튼 */}
-      {!isScheduleConfirmed && (
-        <div className='fixed bottom-0 left-0 z-10 flex w-full transform flex-col items-center gap-[20px] transition-transform duration-300'>
-          <div className='flex w-4xl items-center justify-center rounded-t-[20px] bg-white p-[20px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
-            <button
-              onClick={() => navigate('confirmation')}
-              className='w-full border-none bg-[var(--Blue-Scale-blue-500)] p-[20px] text-2xl text-white'
-            >
-              여행 날짜 확정하기
-            </button>
-          </div>
-        </div>
       )}
-      {/* 케밥 모달 */}
-      {isModalOpen && <KebabModal onClose={toggleModal} />}
-    </div>
+      {/** 여행 제목 수정 모달이 열렸을 때만 렌더링 */}
+      {isUpdateTitleOpen && (
+        <UpdateTitleModal
+          onClose={() => setIsUpdateTitleOpen(false)}
+          onConfirm={(title) => {
+            setIsUpdateTitleOpen(false)
+          }}
+        />
+      )}
+      <div className='flex w-full flex-col pt-5'>
+        <div className='mb-5 flex items-center justify-between px-8'>
+          <button className='border-none' onClick={handleBack}>
+            <GoBack />
+          </button>
+          {/** 케밥 버튼 클릭 시 모달창 열기 */}
+          <button
+            onClick={() => setIsKebabOpen(true)}
+            className='border-none bg-transparent p-0 outline-none focus:outline-none'
+          >
+            <KebabIcon />
+          </button>
+        </div>
+        <div className='flex w-full flex-col gap-15 px-10'>
+          <TripTitle
+            isScheduleConfirmed={isScheduleConfirmed}
+            setIsScheduleConfirmed={setIsScheduleConfirmed}
+          />
+          <SlideTabs isScheduleConfirmed={isScheduleConfirmed} />
+        </div>
+        {/* 여행 날짜 확정 버튼 */}
+        {!isScheduleConfirmed && (
+          <div className='fixed bottom-0 left-0 z-10 flex w-full transform flex-col items-center gap-[20px] transition-transform duration-300'>
+            <div className='flex w-4xl items-center justify-center rounded-t-[20px] bg-white p-[20px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
+              <button
+                onClick={() => navigate('confirmation')}
+                className='w-full border-none bg-[var(--Blue-Scale-blue-500)] p-[20px] text-2xl text-white'
+              >
+                여행 날짜 확정하기
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
