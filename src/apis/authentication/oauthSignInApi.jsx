4a14b8d06b1d913745b0289eba145ce36419521c
gugitgugit <rnwnsgur28@gmail.com>
2025-07-29 14:33:20 +0900

    feat: OAuth 로그인 API URL 수정

--- src/apis/authentication/oauthSignInApi.jsx
+++ src/apis/authentication/oauthSignInApi.jsx
@@ -3,13 +3,7 @@ import api from '@/apis/api'
 /**Oauth 로그인 API */
 const oauthSignInApi = async (platform, code) => {
   try {
-    const response = await api.post(
-      `oauth/sign-in/${platform}`,
-      { code },
-      {
-        headers: { device: 'WEB' },
-      },
-    )
+    const response = await api.post(`oauth/sign-in/${platform}/web`, { code })
     return response.data
   } catch (err) {
     if (err.response?.data?.message) {
