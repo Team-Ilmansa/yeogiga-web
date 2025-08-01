115a5792dcc22eea98108ce19d6d8b6a43bd9184
gugitgugit <rnwnsgur28@gmail.com>
2025-08-01 22:01:36 +0900

    feat: ProtectedRoute에 적용

--- src/components/common/ProtectedRoute.jsx
+++ src/components/common/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from 'react-router-dom'
 import useAuth from '@/hooks/useAuth'
+import SflashScreen from '@/pages/SflashScreen'
 
 const ProtectedRoute = () => {
   const { user, isLoading } = useAuth()
 
-  if (isLoading) return <div>로딩 중...</div>
+  if (isLoading) return <SflashScreen />
   if (!user) return <Navigate to='/signin' replace />
 
   return <Outlet />
