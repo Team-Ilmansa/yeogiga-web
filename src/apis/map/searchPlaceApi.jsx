3ba25b06e3e8849a31e88c8d9039e063ab82b5fc
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-08-05 23:42:40 +0900

    Feature(IL-250): 지도 검색 기능 추가 (#102)

---
+++ src/apis/map/searchPlaceApi.jsx
@@ -0,0 +1,26 @@
+import api from '@/apis/api'
+import { prodBaseUrl } from '@/config/Env'
+
+/**장소 키워드 검색 API */
+const searchPlaceApi = async (keyword) => {
+  try {
+    const response = await api.get(`${prodBaseUrl}places/search`, {
+      params: {
+        place: keyword,
+      },
+    })
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message)
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default searchPlaceApi
