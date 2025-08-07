c2f70ffd291c1d075e26b1be47bf24d1b4d474c6
joohee kim <dev.joohee0916@gmail.com>
2025-08-07 23:38:06 +0900

    feat: 회원탈퇴 버튼 구현

--- src/components/mypage/AccountSettings.jsx
+++ src/components/mypage/AccountSettings.jsx
@@ -1,27 +1,61 @@
 import signOutApi from '@/apis/authentication/signOutApi'
+import deleteUserApi from '@/apis/users/deleteUserApi'
 import useAuth from '@/hooks/useAuth'
-import React from 'react'
+import { useState } from 'react'
+import DeleteTripModal from './modal/DeleteTripModal'
 
 const AccountSettings = () => {
-  const { user, logout } = useAuth()
+  const { logout } = useAuth()
   const style = 'text-lg cursor-pointer'
+
+  /**회원탈퇴 모달창 토글 */
+  const [isDeleteAccountModalOpen, setIsDeleteAccountModalOpen] =
+    useState(false)
 
   /**버튼 클릭 시 로그아웃 */
   const handleSignOut = async () => {
     try {
-      const result = await signOutApi()
+      await signOutApi()
       logout()
     } catch (error) {
       console.error('로그아웃 중 오류 발생: ', error.message)
     }
   }
+
+  /**회원탈퇴 전 확인모달 토글 */
+  const toggleDeleteAccountModal = () => {
+    setIsDeleteAccountModalOpen((prev) => !prev)
+  }
+
+  /**회원탈퇴 API 호출 */
+  const deleteAccount = async (e) => {
+    e.preventDefault()
+    try {
+      await deleteUserApi()
+      setIsDeleteAccountModalOpen(false)
+      logout()
+    } catch (error) {
+      console.error('회원탈퇴 중 오류 발생: ', error.message)
+    }
+  }
+
   return (
     <>
+      {/* /**회원탈퇴 모달 */}
+      {isDeleteAccountModalOpen && (
+        <DeleteTripModal
+          toggleDeleteAccountModal={toggleDeleteAccountModal}
+          deleteAccount={deleteAccount}
+        />
+      )}
+
       <div className={style}>문의하기</div>
       <div onClick={handleSignOut} className={style}>
         로그아웃
       </div>
-      <div className={style}>회원탈퇴</div>
+      <div onClick={toggleDeleteAccountModal} className={style}>
+        회원탈퇴
+      </div>
     </>
   )
 }
