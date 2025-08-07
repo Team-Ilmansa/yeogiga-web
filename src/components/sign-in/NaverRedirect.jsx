40b07583ed4e54dff0c7a00d1144c9bab31112d6
joohee kim <dev.joohee0916@gmail.com>
2025-08-08 00:55:33 +0900

    feat: 탈퇴된 소셜 계정 로그인 시 복구 페이지 연동

--- src/components/sign-in/NaverRedirect.jsx
+++ src/components/sign-in/NaverRedirect.jsx
@@ -24,7 +24,20 @@ const NaverRedirect = () => {
       login({ token: response.data.token.accessToken })
       navigate('/')
     } catch (error) {
-      console.error('네이버 로그인 중 오류 발생:', error.message)
+      const errRes = error.response?.data
+      /**탈퇴 계정 로그인 시 복구 페이지로 이동 */
+      if (errRes?.data?.userId && errRes?.data?.deletionExpiration) {
+        alert('탈퇴된 계정입니다. 계정 복구 페이지로 이동합니다.')
+        navigate('/restore/account', {
+          state: {
+            userId: errRes.data.userId,
+            deletionDate: errRes.data.deletionExpiration,
+          },
+        })
+      } else {
+        alert(`로그인 실패: ${errRes?.message || err.message}`)
+        console.error('로그인 에러: ', err)
+      }
     }
   }
 }
