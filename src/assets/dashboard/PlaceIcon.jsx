d03a778b8968b2f10ea15260bbc8fc6248c4bd36
gugitgugit <rnwnsgur28@gmail.com>
2025-08-09 22:32:08 +0900

    fix: 브라우저 오류 제거

--- src/assets/dashboard/PlaceIcon.jsx
+++ src/assets/dashboard/PlaceIcon.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+/**지도 마커 아이콘 */
 const PlaceIcon = ({ className }) => {
   return (
     <div>
@@ -13,9 +12,9 @@ const PlaceIcon = ({ className }) => {
         <path
           d='M15 11.3744C18.5318 12.0688 21 13.6547 21 15.5C21 17.9853 16.5228 20 11 20C5.47715 20 1 17.9853 1 15.5C1 13.6547 3.46819 12.0688 7 11.3744M11 15V7M11 7C12.6569 7 14 5.65685 14 4C14 2.34315 12.6569 1 11 1C9.34315 1 8 2.34315 8 4C8 5.65685 9.34315 7 11 7Z'
           stroke='#8287FF'
-          stroke-width='2'
-          stroke-linecap='round'
-          stroke-linejoin='round'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
         />
       </svg>
     </div>
