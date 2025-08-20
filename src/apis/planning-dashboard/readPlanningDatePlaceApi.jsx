2d885788c0eb267b2f46f6fb437e687933562624
gugitgugit <rnwnsgur28@gmail.com>
2025-08-20 22:24:56 +0900

    feat: 확정 후 API 구현

---
+++ src/apis/planning-dashboard/readPlanningDatePlaceApi.jsx
@@ -0,0 +1,22 @@
+import api from '@/apis/api'
+import { prodBaseUrl } from '@/config/Env'
+
+/**확정 후 일정 조회 API */
+const readPlanningDatePlaceApi = async (tripId) => {
+  try {
+    const response = await api.get(`trip/${tripId}/day-place/places`)
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw new Error(err.response.data.message)
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default readPlanningDatePlaceApi
