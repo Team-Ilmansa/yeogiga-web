7eb194cbe1e0dde4d9ed98fb94892406d2ffb3c5
gugitgugit <rnwnsgur28@gmail.com>
2025-09-05 11:38:09 +0900

    feat: 임시 저장 이미지 조회 API 구현

---
+++ src/apis/image/readTemporaryImagesApi.jsx
@@ -0,0 +1,23 @@
+import api from '@/apis/api'
+
+/**여행 일차 별 임시 저장 이미지 조회 API */
+const readTemporaryImagesApi = async (tripId, tripDayPlaceId) => {
+  try {
+    const response = await api.get(
+      `trip/${tripId}/day-place/${tripDayPlaceId}/temp-images`,
+    )
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw err.response.data
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default readTemporaryImagesApi
