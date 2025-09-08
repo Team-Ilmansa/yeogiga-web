47d46ed67f10a5ad2ac72dde01bfac5485c596ae
Koo Jun Hyeok <164734665+gugitgugit@users.noreply.github.com>
2025-09-08 20:50:52 +0900

    Feature(IL-293): 임시 저장 이미지 매칭 (#135)

---
+++ src/apis/image/matchTemporaryImagesApi.jsx
@@ -0,0 +1,23 @@
+import api from '@/apis/api'
+
+/**임시 등록 이미지 매칭 API */
+const matchTemporaryImagesApi = async (tripId, tripDayPlaceId) => {
+  try {
+    const response = await api.post(
+      `trip/${tripId}/day-place/${tripDayPlaceId}/images/assign`,
+    )
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw err.response.data
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default matchTemporaryImagesApi
