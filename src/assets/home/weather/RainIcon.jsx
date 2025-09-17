7860aa35be72cec777ca04913df7462f6367db3c
gugitgugit <rnwnsgur28@gmail.com>
2025-09-17 18:10:19 +0900

    feat: 아이콘 및 배경 파일 추가

---
+++ src/assets/home/weather/RainIcon.jsx
@@ -0,0 +1,19 @@
+const RainIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='22'
+    height='23'
+    viewBox='0 0 22 23'
+    fill='none'
+  >
+    <path
+      d='M15 11.5V19.5M7 11.5V19.5M11 13.5V21.5M19 14.7422C20.206 13.935 21 12.5602 21 11C21 8.65643 19.2085 6.73129 16.9203 6.51937C16.4522 3.67213 13.9798 1.5 11 1.5C8.02024 1.5 5.54781 3.67213 5.07974 6.51937C2.79151 6.73129 1 8.65643 1 11C1 12.5602 1.79401 13.935 3 14.7422'
+      stroke='white'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
+
+export default RainIcon
