6bb5ec179a0268e918701e515254ef0be5a5e5c6
juiuj <151600782+juiuj@users.noreply.github.com>
2025-09-22 00:27:31 +0900

    Feature(IL-323): 정산 내역 UI 수정 및 API 연동 (#146)

--- src/pages/SettlementBook.jsx
+++ src/pages/SettlementBook.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
-const AccountBook = () => {
+const SettlementBook = () => {
   const { tripId } = useParams()
   return <div>Trip {tripId} 가계부</div>
 }
 
-export default AccountBook
+export default SettlementBook
