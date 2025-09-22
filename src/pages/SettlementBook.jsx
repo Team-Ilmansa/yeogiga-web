9c32199a931183b39f19f5d536650e7f30c9da2d
joohee kim <dev.joohee0916@gmail.com>
2025-09-22 23:52:57 +0900

    feat: 정산 내역 전체 조회 페이지 구현

--- src/pages/SettlementBook.jsx
+++ src/pages/SettlementBook.jsx
@@ -1,14 +1,19 @@
 import GoBack from '@/assets/sign-up/GoBack'
-import UnsettledTitle from '@/components/account-book/UnsettledTitle'
-import React from 'react'
+import FixedActionBar from '@/components/common/FixedActionBar'
+import SettlementBox from '@/components/settlement/SettlementBox'
+import SettlementTabs from '@/components/settlement/SettlementTabs'
+import UnsettledTitle from '@/components/settlement/UnsettledTitle'
+import React, { useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-// TODO: 뒤로가기버튼, 미정산내역, 날짜별 정산내역(기타 포함), 날짜별 박스
+
 const SettlementBook = () => {
   const { tripId } = useParams()
   const navigate = useNavigate()
+  const [view, setView] = useState({ key: 'ALL' })
 
   /** 뒤로 가기 버튼 */
   const handleBack = () => navigate(-1)
+
   return (
     <>
       <div className='flex w-full flex-col pt-5'>
@@ -19,10 +24,26 @@ const SettlementBook = () => {
           </button>
         </div>
         {/**타이틀 및 슬라이딩 날짜 및 내역 탭 */}
-        <div className='flex w-full flex-col gap-15 px-10'>
+        <div className='flex w-full flex-col gap-8 px-10'>
           {/**타이틀*/}
           <UnsettledTitle />
           {/**미정산 내역 및 날짜 탭 */}
+          <SettlementTabs onChange={setView} />
+
+          {/**날짜별 내역 박스 */}
+          <SettlementBox mode={view.key} date={view.date} />
+          {/**내역 추가하기 버튼 */}
+          <FixedActionBar className='flex justify-center'>
+            <div className='flex w-4xl items-center justify-center rounded-t-[20px] bg-white p-[20px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
+              <button
+                button
+                onClick={() => navigate(`/trip/${tripId}/settlement/add`)}
+                className='flex w-full items-center justify-center gap-2 rounded-lg border-none bg-[var(--Blue-Scale-blue-500)] p-[20px] text-2xl text-white'
+              >
+                내역 추가하기
+              </button>
+            </div>
+          </FixedActionBar>
         </div>
       </div>
     </>
