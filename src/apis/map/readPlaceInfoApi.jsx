7cea9d9187ebfdf737a915ed3adecf9f64768034
gugitgugit <rnwnsgur28@gmail.com>
2025-10-02 21:27:21 +0900

    feat: 목적지 상세 정보 불러오기 API 구현

---
+++ src/apis/map/readPlaceInfoApi.jsx
@@ -0,0 +1,23 @@
+import api from '@/apis/api'
+
+/**특정 일차 목적지 상세 정보 불러오기 API */
+const readPlaceInfoApi = async (tripId, tripDayPlaceId) => {
+  try {
+    const response = await api.get(
+      `trip/${tripId}/day-place/${tripDayPlaceId}/places`,
+    )
+    return response.data
+  } catch (err) {
+    if (err.response?.data?.message) {
+      throw err.response.data
+    } else if (err.response) {
+      throw new Error(`오류 발생 (status: ${err.response.status})`)
+    } else if (err.request) {
+      throw new Error('서버로부터 응답이 없습니다.')
+    } else {
+      throw new Error('요청 중 알 수 없는 오류가 발생했습니다.')
+    }
+  }
+}
+
+export default readPlaceInfoApi
