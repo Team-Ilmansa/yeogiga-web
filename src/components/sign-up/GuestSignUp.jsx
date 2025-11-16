82e62aa7b16e29345f6e57757e8a072e13e6f271
joohee kim <dev.joohee0916@gmail.com>
2025-11-16 23:11:09 +0900

    feat: 약관동의 여부 및 버튼 비활성 로직 추가

--- src/components/sign-up/GuestSignUp.jsx
+++ src/components/sign-up/GuestSignUp.jsx
@@ -11,42 +11,34 @@ import putGuestApi from '@/apis/authentication/putGuestApi'
 const GuestSignUp = () => {
   const navigate = useNavigate()
 
-  /** 단계: 1=약관동의, 2=닉네임, 3=완료 */
   const [step, setStep] = useState(1)
 
   /** 닉네임 유효성 여부 */
   const [isNicknameVerified, setIsNicknameVerified] = useState(false)
+  const [isTermsAgreed, setIsTermsAgreed] = useState(false)
 
   /** 입력값 */
   const [nickname, setNickname] = useState('')
 
-  /** API 요청 진행 여부 (중복 클릭 방지용) */
   const [isSubmitting, setIsSubmitting] = useState(false)
 
-  /** 뒤로 가기 버튼 */
   const handleBack = () => {
-    if (step === 1) {
-      navigate(-1)
-    } else {
-      setStep((prev) => prev - 1)
-    }
+    if (step === 1) navigate(-1)
+    else setStep((prev) => prev - 1)
   }
 
-  /** 하단 버튼 */
+  /** 다음 단계 버튼 */
   const handleStepButton = async () => {
-    // 완료 단계: 로그인 페이지로 이동
     if (step === 3) {
       navigate('/signin')
       return
     }
 
-    // 닉네임 단계: 게스트 가입 API 호출 후 다음 단계로
     if (step === 2) {
       if (isSubmitting) return
       try {
         setIsSubmitting(true)
-        const result = await putGuestApi({ nickname })
-        console.log(result)
+        await putGuestApi({ nickname })
         setStep(3)
       } catch (err) {
         alert(
@@ -54,7 +46,6 @@ const GuestSignUp = () => {
             err.message ||
             '게스트 가입에 실패했습니다.',
         )
-        console.error(err)
       } finally {
         setIsSubmitting(false)
       }
@@ -66,7 +57,12 @@ const GuestSignUp = () => {
 
   /** 단계별 컴포넌트 */
   const stepComponentMap = {
-    1: <TermsAgreement />,
+    1: (
+      <TermsAgreement
+        isAgreed={isTermsAgreed}
+        onChangeAgree={setIsTermsAgreed}
+      />
+    ),
     2: (
       <RegisterNickname
         setIsNicknameVerified={setIsNicknameVerified}
@@ -76,11 +72,11 @@ const GuestSignUp = () => {
     3: <SignUpConfirmation nickname={nickname} />,
   }
 
-  const isDisabled = step === 2 && !isNicknameVerified
+  const isDisabled =
+    (step === 1 && !isTermsAgreed) || (step === 2 && !isNicknameVerified)
 
   return (
     <div className='flex w-full flex-col pt-5'>
-      {/** 뒤로 가기 버튼 */}
       <div>
         <button
           className='text-bold my-5 border-none px-8'
@@ -91,9 +87,10 @@ const GuestSignUp = () => {
       </div>
 
       {stepComponentMap[step]}
-      {/**다음 단계 버튼 */}
+
       <div className='fixed bottom-0 left-0 flex w-full flex-col items-center gap-[20px]'>
         {step < 3 && <StepIndicator step={step} totalSteps={3} />}
+
         <div className='flex w-4xl items-center justify-center rounded-t-[20px] p-[20px] shadow-[0_0_4px_rgba(0,0,0,0.10)]'>
           <button
             onClick={handleStepButton}
